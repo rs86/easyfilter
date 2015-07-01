bfeb57f3bd3f4a0b646a80f4df90d30ae042b098
Rodrigo Stevaux <rodrigo@stevaux.com>
2015-07-01 10:40:47 -0300

    Adds support for using filters only on given columns

--- easyfilter.js
+++ easyfilter.js
@@ -1,63 +1,69 @@
-// TODO: Exibir filtros só em <th> específicos
-
 function testPatternAgainstValue(p, v) {
-  // TODO: Aceitar expressões matemáticas nos filtros (<, >)
   return !v.includes(p);
 }
 
 function updateFilters () {
-  // TODO: atualizar só o Filterable pai do elemento que disparou o evento
-  $('.filterable').each(function() {
+    console.log('updateFilters');
+    
+    $('table.ef').each(function(i, table) {
+        var patterns = $(table).find('thead tr.ef-filter-row th').map(function (j, columnFilter) {
+            if ($(columnFilter).hasClass('ef-column-filter')) {
+                var inputBox = $(columnFilter).find('input');
+                return inputBox.val();
+            } else {
+                return "";
+            }
+        }).toArray();      
+       
+        $(table).find('tbody tr').each(function (j, row) {
+           
 
-    // Cria array com os filtros.
-    var patterns = $(this)
-                      .find('.filterable-filter-row th input')
-                      .map(function() {
-                        return $(this).val();
-                      }).toArray();
+            var values = $(row).find('td').map(function (k, cell) {
+                return $(cell).text();
+            }).toArray();
 
-    // Itera por cada linha da tabela e testa se passa no filtro.
-    $(this).find('tbody tr').each(function() {
-      // Mostra a linha
-      $(this).show();
+            patternsAndValues = _.zip(patterns, values);
+            results = _.map(patternsAndValues, function (e) {
+                return testPatternAgainstValue(e[0], e[1]);
+            });
 
-      // Mapeia cada célula da linha para um elemento na array
-      var values = $(this)
-                      .children()
-                      .map(function() {
-                        return $(this).text();
-                      });
+            if (_.any(results)) {
+                $(row).hide();
+            
+            } else {
+                $(row).show();
+            }
 
-      var patternValuePair = _.zip(patterns, values);
-      var patternMatch = _.map(patternValuePair, testPatternAgainstValue);
+        });
 
-      // Se algum filtro não passar....
-      if (_.any(patternMatch)) {
-        $(this).hide();
-      }
 
-    }); // $(this).find('tbody tr').each(function() {
-  });
+     });
+
+    
 }
 
-// A rotina seleciona todas as tabelas com .filterable.
-// Cada tabela com filtros deve ter o <thead> bem especificado.
-// Nós criaremos uma nova <tr> com class .filterable-filter-row
-// dentro do <thead>, e colocaremos uma <th> para cada coluna da tabela.
-// Dentro de cada <th>, colocaremos um <input> que será usado para os filtros.
-function setupFilters () {
-  $("table.filterable").each(function() {
-    table = $(this);
-    tableHead = table.find('thead');
-    tableHead.append("<tr class='filterable-filter-row'></tr>")
-    tableHead.find('th').each(function() {
-      $('<th><input></input></th>')
-        .appendTo(tableHead.find('.filterable-filter-row'))
-        .change(function() {
-          updateFilters();
-        });
+function setupTable(t) {
+
+    $(t).find('thead')
+        .append("<tr class='ef-filter-row'></tr>");
+
+    $(t).find('thead tr.ef-header th').each(function (i, elem) {
+        if ($(elem).hasClass('ef-column')) {
+            $(t).find('thead tr.ef-filter-row').append("<th class='ef-column-filter'><input class='ef-input'></input></th>"); 
+        } else {
+            $(t).find('thead tr.ef-filter-row').append("<th class='ef-column-filter-nofilter'></th>"); 
+        }
     });
-  });
+
+    $(".ef-input").change(function () {
+       updateFilters(); 
+    });
+}
+
+function setupFilters() {
+    $("table.ef").each(function () {
+        setupTable(this);
+    });
 }
 
 $(document).ready(setupFilters);
