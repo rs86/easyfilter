816e0ce430e608901bd0fe6c6d195b5470714c32
Rodrigo Stevaux <rodrigo@stevaux.com>
2015-07-01 14:23:53 -0300

    Begins to add support to different pattern matching strategies, move logic from CSS classes to CSS attributes

--- easyfilter.js
+++ easyfilter.js
@@ -1,45 +1,43 @@
-function testPatternAgainstValue(p, v) {
-  return !v.includes(p);
+function testPatternAgainstValue(v, p) {
+  if (p.length == 0)
+    return true;
+  return v.includes(p);
+}
+
+function updateTable(table) {
+
+    var cellsWithPatterns = $(table).find('thead tr.ef-filter-row th');
+    var patterns = cellsWithPatterns.map(function(idx, cell) {;
+        if ($(cell).children('input').length > 0)
+            return  {
+                        pattern: $(cell).children('input').val(),
+                        logic: $(cell).children('input').attr('filter-logic')
+                    };
+        return "";
+    }).toArray();
+
+    $(table).find('tbody tr').each(function (j, row) {
+        var rowCells = $(row).find('td');
+        $(row).show();
+        for (var i = 0; i < rowCells.length; i++) {
+            var text = $(rowCells[i]).text();
+            var patt = patterns[i];
+            if (patt != "") {
+                console.log(text, patt['pattern'], testPatternAgainstValue(text, patt['pattern']));
+
+                if (!testPatternAgainstValue(text, patt['pattern'])) {
+                    $(row).hide();
+                }
+            }
+        }
+        });
 }
 
 function updateFilters () {
     console.log('updateFilters');
-    
     $('table.ef').each(function(i, table) {
-        var patterns = $(table).find('thead tr.ef-filter-row th').map(function (j, columnFilter) {
-            if ($(columnFilter).hasClass('ef-column-filter')) {
-                var inputBox = $(columnFilter).find('input');
-                return inputBox.val();
-            } else {
-                return "";
-            }
-        }).toArray();      
-       
-        $(table).find('tbody tr').each(function (j, row) {
-           
-
-            var values = $(row).find('td').map(function (k, cell) {
-                return $(cell).text();
-            }).toArray();
-
-            patternsAndValues = _.zip(patterns, values);
-            results = _.map(patternsAndValues, function (e) {
-                return testPatternAgainstValue(e[0], e[1]);
-            });
-
-            if (_.any(results)) {
-                $(row).hide();
-            
-            } else {
-                $(row).show();
-            }
-
-        });
-
-
-     });
-
-    
+        updateTable(table);
+    });
 }
 
 function setupTable(t) {
@@ -48,15 +46,17 @@ function setupTable(t) {
         .append("<tr class='ef-filter-row'></tr>");
 
     $(t).find('thead tr.ef-header th').each(function (i, elem) {
-        if ($(elem).hasClass('ef-column')) {
-            $(t).find('thead tr.ef-filter-row').append("<th class='ef-column-filter'><input class='ef-input'></input></th>"); 
+        if ($(elem).attr('is-filter')) {
+            $(t).find('thead tr.ef-filter-row')
+                .append("<th><input filter-logic='" + $(elem).attr('filter-logic') + "'></input></th>");
         } else {
-            $(t).find('thead tr.ef-filter-row').append("<th class='ef-column-filter-nofilter'></th>"); 
+            $(t).find('thead tr.ef-filter-row')
+                .append("<th></th>");
         }
     });
 
-    $(".ef-input").change(function () {
-       updateFilters(); 
+    $(".ef thead tr th input").change(function () {
+       updateFilters();
     });
 }
 
